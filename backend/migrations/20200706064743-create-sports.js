29acb43a899de6fef2c6934d3eeffcd4e8525ca9
Dyram Val Alec Meyn <dyram.meyn@codingmart.com>
2020-07-06 12:25:09 +0530

    Initial Commit with Login and Signup done

---
+++ backend/migrations/20200706064743-create-sports.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      slot: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sports');
+  }
+};
